50f01612edfc4774d06be1758e21f643c374fd1d
colincharles <42841337+colincharles@users.noreply.github.com>
2018-12-18 15:57:38 -0600

    Update moneyPitches.R

--- moneyPitches.R
+++ moneyPitches.R
@@ -35,17 +35,19 @@ moneyPitches = function(playerid){
 }
 
 # 2018 Kershaw playerid = 2036
-
 kershaw = moneyPitches(playerid = 2036)
-
 kershaw
 
-# # Find out what working directory (save location) you are currently in
-# getwd()
-# 
-# # Save the file to a csv in your current working directory
-# write.csv(kershaw, file = "Kershaw Money Pitches 2018.csv", row.names = F)
-# 
+#####
+#
+### Now apply the moneyPitches() function to multiple pitchers
+#
+#####
+
+# Map of 2018 MLB Player Names and IDs
+master = read.csv("http://crunchtimebaseball.com/master.csv") %>%
+  mutate(fg_name = as.character(fg_name),
+         fg_id = as.numeric(as.character(fg_id)))
 
 #####
 #
@@ -68,34 +70,53 @@ colnames(df) = df[1,]
 df = df %>%
   slice(-1) %>%
   mutate(first_name = sapply(strsplit(Name, split=' ', fixed=TRUE), function(x) (x[1])),
-         last_name = sapply(strsplit(Name, split=' ', fixed=TRUE), function(x) (x[2])))
+         last_name = sapply(strsplit(Name, split=' ', fixed=TRUE), function(x) (x[2]))) %>%
+  # Combine the pitch leaderboards with the Fangraphs ID for scraping game logs
+  left_join(df, master[,c("fg_name","fg_id")], by = c("Name" = "fg_name")) %>%
+  mutate(fg_id = as.numeric(as.character(fg_id)))
 
-# Run this to initialize the Chadwick Bureau Register look up
-playerid_lookup("Garcia", "Karim")
-
-
-# initialize an empty dataframe
-data = NULL
-for(i in 1:nrow(df)){
-  # Grab player_id using first and last names. Trevor Bauer
-  # had 2 entries, but one had a bunch of NAs. Didn't look too much into it
-  # but will do more checking later
-  player = playerid_lookup(df$last_name[i], df$first_name[i]) %>%
-    filter(!is.na(fangraphs_id))
+                             
+data = map_df(unique(df$fg_id), function(i){
+  moneyPitches(i)
+})
+                                 
+data = right_join(master[,c("fg_name", "fg_id")], data) %>%
+  rename(Name = fg_name) %>%
+  select(-fg_id) %>%
+  arrange(desc(VPR))
   
-  # grab Fangrpahs ID
-  id = player$fangraphs_id
-  
-  # Extract pitch info and add in player name and ID for filtering
-  player1 = moneyPitches(id)
-  player1$Name = paste(df$first_name[i], df$last_name[i])
-  player1$ID = id
-  
-  data = rbind(data, player1)
-}
 
-# Extract the pitches which exceed the "money pitch" criteria
-# 40% O-swing, 40% Zone, 15% Swstr
 data %>%
   dplyr::filter(`O-Swing%` > 40 & `Zone%` > 40 & `SwStr%` > 15) %>%
-  as.data.frame()
+  as.data.frame()   
+          
+                            
+                            
+## Old code (not ready to delete)
+# 
+# # initialize an empty dataframe
+# data = NULL
+# for(i in 1:nrow(df)){
+#   # Grab player_id using first and last names. Trevor Bauer
+#   # had 2 entries, but one had a bunch of NAs. Didn't look too much into it
+#   # but will do more checking later
+#   player = playerid_lookup(df$last_name[i], df$first_name[i]) %>%
+#     filter(!is.na(fangraphs_id))
+#   
+#   # grab Fangrpahs ID
+#   id = player$fangraphs_id
+#   
+#   # Extract pitch info and add in player name and ID for filtering
+#   player1 = moneyPitches(id)
+#   player1$Name = paste(df$first_name[i], df$last_name[i])
+#   player1$ID = id
+#   
+#   data = rbind(data, player1)
+# }
+# 
+# # Extract the pitches which exceed the "money pitch" criteria
+# # 40% O-swing, 40% Zone, 15% Swstr
+# data %>%
+#   dplyr::filter(`O-Swing%` > 40 & `Zone%` > 40 & `SwStr%` > 15) %>%
+#   as.data.frame()                            
+                            
